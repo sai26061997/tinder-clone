1b8f61d0f1fc7d740643054782a7ede4a8768498
sai <sai26061997@gmail.com>
2020-11-20 00:04:18 +0530

    bug

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const port=process.env.PORT || 9000;
 
 //middleware
 app.use(express.json());
-
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*'),
+    res.setHeader('Access-Control-Allow-Header','*')
+})
 
 //db config
 const conurl="mongodb+srv://admin:m8lvcMWCqmlVQBQV@cluster0.c335t.mongodb.net/tiktok?retryWrites=true&w=majority"
