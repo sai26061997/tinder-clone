ab57f1952021ecac28b89889588a8fbcea70d7d8
sai <sai26061997@gmail.com>
2020-11-20 12:35:29 +0530

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const port=process.env.PORT || 9000;
 app.use(express.json());
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*'),
-    res.setHeader('Access-Control-Allow-Header','*')
+    res.setHeader('Access-Control-Allow-Headers','*'),
+    next()
 })
 
 //db config
