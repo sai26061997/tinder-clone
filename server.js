1c7f69f8130b7b8574f17aa3f82251d0887ae508
sai <sai26061997@gmail.com>
2023-05-12 02:02:01 +0100

    mongodb

--- server.js
+++ server.js
@@ -1,52 +1,56 @@
-//import express from "express";
-const express = require('express');
-const mongoose=require('mongoose');
-const cards =require("./dbCards");
-const cors= require('cors');
+import express from "express"
+import mongoose from "mongoose"
+//import data from "./data.js"
+import Videos from "./dbmodel.js"
 
+//app config 
+const app=express()
+const port=process.env.PORT || 9000;
 
-
-//App Config
-
-const app =express()
-const port= process.env.PORT ||8001
-const connection_url='mongodb+srv://admin1:CtthoHezLed0Ou9a@cluster0.mzetevw.mongodb.net/?retryWrites=true&w=majority'
-
-//Middleware
+//middleware
 app.use(express.json());
-app.use(cors());
-//DB Config
-mongoose.connect(connection_url,{
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Header','*');
+    next()
+})
+
+//db config
+const conurl="mongodb+srv://sai:pxs5JJlO4H0zJsJR@cluster0.grnv3bb.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(conurl,{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useUnifiedTopology:true,
+    useUnifiedTopology:true
 })
-//API endpoints 
-app.get('/',(req,res)=>res.status(200).send( "hello"));
-app.post("/tinder/card",(req,res)=>{
-    const dbcards= req.body
-    console.log(dbcards);   
-        cards.create(dbcards,(err,data)=>{
+
+//api endpoints 
+app.get("/",(req,res)=>res.status(200).send("hellow World"))
+app.get("/v1/posts",(req,res)=>res.status(200).send(data))
+
+app.get("/v2/posts",(req,res)=>{
+    Videos.find((err,data)=>{
         if(err){
             res.status(500).send(err)
-        } else {
-            res.status(201).send(data)
+        }else{
+            res.status(200).send(data)
         }
-    });
+    
+    })
+})
 
-});
+app.post("/v2/posts",(req,res)=>{
+const dbdata=req.body
+Videos.create(dbdata,(err,data)=>{
+    if(err){
+        res.status(500).send(err)
+    }else{
+        res.status(201).send(data)
+    }
 
-app.get("/tinder/card",(req,res)=>{
-cards.find((err, data) => {
-        if (err) {
-           
-            res.status(500).send(err);
-        } else {
-            res.status(200).send(data);
-        }
-    });
+})
 
-});
-//Listeners
+})
 
-app.listen(port,()=>console.log(`Listening at port ${port}` ));
+
+//listener
+app.listen(port,()=>console.log("listenting on"+port))
